b63ca2c672ff3dfe53c6632671dd38ed363945d9
Wu Rong Hua <LoyaltyWu@users.noreply.github.com>
2017-09-24 02:16:33 +0800

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,319 +1,277 @@
 /*===============================================================================
-Copyright (c) 2016 PTC Inc. All Rights Reserved.
-
 Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc. All Rights Reserved.
 
-Vuforia is a trademark of PTC Inc., registered in the United States and other
-countries.
+Vuforia is a trademark of QUALCOMM Incorporated, registered in the United States 
+and other countries. Trademarks of QUALCOMM Incorporated are used with permission.
 ===============================================================================*/
 
-package com.vuforia.samples.VuforiaSamples.app.ImageTargets;
+package com.qualcomm.vuforia.samples.SampleApplication.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Vector;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
-
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
-import android.opengl.Matrix;
+import android.content.res.AssetManager;
 import android.util.Log;
 
-import com.vuforia.Device;
-import com.vuforia.Matrix44F;
-import com.vuforia.State;
-import com.vuforia.Tool;
-import com.vuforia.Trackable;
-import com.vuforia.TrackableResult;
-import com.vuforia.Vuforia;
-import com.vuforia.samples.SampleApplication.SampleAppRenderer;
-import com.vuforia.samples.SampleApplication.SampleAppRendererControl;
-import com.vuforia.samples.SampleApplication.SampleApplicationSession;
-import com.vuforia.samples.SampleApplication.utils.CubeShaders;
-import com.vuforia.samples.SampleApplication.utils.LoadingDialogHandler;
-import com.vuforia.samples.SampleApplication.utils.SampleApplication3DModel;
-import com.vuforia.samples.SampleApplication.utils.SampleUtils;
-import com.vuforia.samples.SampleApplication.utils.Teapot;
-import com.vuforia.samples.SampleApplication.utils.Texture;
 
 
-// The renderer class for the ImageTargets sample.
-public class ImageTargetRenderer implements GLSurfaceView.Renderer, SampleAppRendererControl
+public class Banana extends MeshObject
 {
-    private static final String LOGTAG = "ImageTargetRenderer";
 
-    private SampleApplicationSession vuforiaAppSession;
-    private ImageTargets mActivity;// 看到没有！是可以直接用的(slime类通信问题，反过来)
-    private SampleAppRenderer mSampleAppRenderer;
+    private static final String LOGTAG = "Banana";
 
-    private Vector<Texture> mTextures;
+    private Buffer mVertBuff;
+    private Buffer mTexCoordBuff;
+    private Buffer mNormBuff;
+    private Buffer mIndBuff;
 
-    private int shaderProgramID;
-    private int vertexHandle;
-    private int textureCoordHandle;
-    private int mvpMatrixHandle;
-    private int texSampler2DHandle;
+    private int indicesNumber = 0;
+    private int verticesNumber = 0;
 
-    private Teapot mTeapot;
+    private AssetManager assetManager;
 
-    private float kBuildingScale = 0.012f;
-    private SampleApplication3DModel mBuildingsModel;
-
-    private boolean mIsActive = false;
-    private boolean mModelIsLoaded = false;
-
-    private static final float OBJECT_SCALE_FLOAT = 0.003f;
-
-    // biatch begin
-    // slime直接在这边粗暴搞。
-
-    public float x = 0;
-    public float y = 0;
-    public float z = 0;
-
-    // biatch
-    public boolean getFucked(){
-        return mModelIsLoaded;
-    }
-    // biatch
-
-    public ImageTargetRenderer(ImageTargets activity, SampleApplicationSession session)
+    public Banana(AssetManager inputassetManager)
     {
-        mActivity = activity;// 在这边实现通信！！？？？我好像是要在那边实现通信来告诉你诶！！。。。。。// 看到没有！是可以直接用的(slime类通信问题，反过来)
-        vuforiaAppSession = session;
-        // SampleAppRenderer used to encapsulate the use of RenderingPrimitives setting
-        // the device mode AR/VR and stereo mode
-        mSampleAppRenderer = new SampleAppRenderer(this, mActivity, Device.MODE.MODE_AR, false, 0.01f , 5f);
-    }
+        this.assetManager = inputassetManager;
+        setVerts();
+        setTexCoords();
+        setNorms();
 
-    // Called to draw the current frame.
-    @Override
-    public void onDrawFrame(GL10 gl)
-    {
-        if (!mIsActive)
-            return;
-
-        // Call our function to render content from SampleAppRenderer class
-        mSampleAppRenderer.render();
     }
 
 
-    public void setActive(boolean active)
-    {
-        mIsActive = active;
+    double[] banana_VERTS;
+    double[] banana_TEX_COORDS;
+    double[] banana_NORMS;
 
-        if(mIsActive)
-            mSampleAppRenderer.configureVideoBackground();
-    }
+    InputStream inputFile = null;
 
 
-    // Called when the surface is created or recreated.
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config)
-    {
-        Log.d(LOGTAG, "GLRenderer.onSurfaceCreated");
-
-        // Call Vuforia function to (re)initialize rendering after first use
-        // or after OpenGL ES context was lost (e.g. after onPause/onResume):
-        vuforiaAppSession.onSurfaceCreated();
-
-        mSampleAppRenderer.onSurfaceCreated();
-    }
-
-
-    // Called when the surface changed size.
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        Log.d(LOGTAG, "GLRenderer.onSurfaceChanged");
-
-        // Call Vuforia function to handle render surface size changes:
-        vuforiaAppSession.onSurfaceChanged(width, height);
-
-        // RenderingPrimitives to be updated when some rendering change is done
-        mSampleAppRenderer.onConfigurationChanged(mIsActive);
-
-        initRendering();
-    }
-
-
-    // Function for initializing the renderer.
-    private void initRendering()
-    {
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, Vuforia.requiresAlpha() ? 0.0f
-                : 1.0f);
-
-        for (Texture t : mTextures)
+    private int loadVertsFromModel(String fileName)
+            throws IOException
         {
-            GLES20.glGenTextures(1, t.mTextureID, 0);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, t.mTextureID[0]);
-            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA,
-                t.mWidth, t.mHeight, 0, GLES20.GL_RGBA,
-                GLES20.GL_UNSIGNED_BYTE, t.mData);
+            try
+            {
+                inputFile = assetManager.open(fileName);
+
+                BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputFile));
+
+                String line = reader.readLine();
+
+                //ex.24168 3 floatsdata in 1 line
+                int floatsToRead = Integer.parseInt(line);
+                //int floatsToRead = 1000;
+
+                banana_VERTS = new double[3*floatsToRead];
+
+
+                for (int i = 0; i < floatsToRead; i++)
+                {
+
+                    String curline = reader.readLine();
+                    if( curline.indexOf('/') >= 0 ){
+                        i--;
+                        continue;
+                    }
+
+                    //split 1 line to 3 data
+                    String floatStrs[] = curline.split(",");
+
+                    banana_VERTS[3*i] = Float.parseFloat(floatStrs[0]);
+                    banana_VERTS[3*i+1] = Float.parseFloat(floatStrs[1]);
+                    banana_VERTS[3*i+2] = Float.parseFloat(floatStrs[2]);
+                    Log.d(LOGTAG, ""+i);
+                }
+
+                return floatsToRead;
+
+
+            } finally
+            {
+                if (inputFile != null)
+                    inputFile.close();
+            }
+        }
+    private int loadTexCoordsFromModel(String fileName)
+            throws IOException
+        {
+            try
+            {
+                inputFile = assetManager.open(fileName);
+
+                BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputFile));
+
+                String line = reader.readLine();
+
+                //ex.24168 3 floatsdata in 1 line
+                int floatsToRead = Integer.parseInt(line);
+                //int floatsToRead = 1000;
+
+                banana_TEX_COORDS = new double[2*floatsToRead];
+
+
+                for (int i = 0; i < floatsToRead; i++)
+                {
+
+                    String curline = reader.readLine();
+                    if( curline.indexOf('/') >= 0 ){
+                        i--;
+                        continue;
+                    }
+
+                    //split 1 line to 2 data
+                    String floatStrs[] = curline.split(",");
+
+                    banana_TEX_COORDS[2*i] = Float.parseFloat(floatStrs[0]);
+                    banana_TEX_COORDS[2*i+1] = Float.parseFloat(floatStrs[1]);
+                    //banana_TEX_COORDS[3*i+2] = Float.parseFloat(floatStrs[2]);
+                }
+
+                return floatsToRead;
+
+
+            } finally
+            {
+                if (inputFile != null)
+                    inputFile.close();
+            }
         }
 
-        shaderProgramID = SampleUtils.createProgramFromShaderSrc(
-            CubeShaders.CUBE_MESH_VERTEX_SHADER,
-            CubeShaders.CUBE_MESH_FRAGMENT_SHADER);
+    private int loadNormsFromModel(String fileName)
+            throws IOException
+        {
+            try
+            {
+                inputFile = assetManager.open(fileName);
 
-        vertexHandle = GLES20.glGetAttribLocation(shaderProgramID,
-            "vertexPosition");
-        textureCoordHandle = GLES20.glGetAttribLocation(shaderProgramID,
-            "vertexTexCoord");
-        mvpMatrixHandle = GLES20.glGetUniformLocation(shaderProgramID,
-            "modelViewProjectionMatrix");
-        texSampler2DHandle = GLES20.glGetUniformLocation(shaderProgramID,
-            "texSampler2D");
+                BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(inputFile));
 
-        if(!mModelIsLoaded) {
-            mTeapot = new Teapot();
+                String line = reader.readLine();
 
-            try {
-                mBuildingsModel = new SampleApplication3DModel();
-                mBuildingsModel.loadModel(mActivity.getResources().getAssets(),
-                        "ImageTargets/Buildings.txt");
-                mModelIsLoaded = true;
-            } catch (IOException e) {
-                Log.e(LOGTAG, "Unable to load buildings");
+                //ex.24168 3 floatsdata in 1 line
+                int floatsToRead = Integer.parseInt(line);
+                //int floatsToRead = 1000;
+
+                banana_NORMS = new double[3*floatsToRead];
+
+
+                for (int i = 0; i < floatsToRead; i++)
+                {
+
+                    String curline = reader.readLine();
+                    if( curline.indexOf('/') >= 0 ){
+                        i--;
+                        continue;
+                    }
+
+                    //split 1 line to 3 data
+                    String floatStrs[] = curline.split(",");
+
+                    banana_NORMS[3*i] = Float.parseFloat(floatStrs[0]);
+                    banana_NORMS[3*i+1] = Float.parseFloat(floatStrs[1]);
+                    banana_NORMS[3*i+2] = Float.parseFloat(floatStrs[2]);
+                }
+
+                return floatsToRead;
+
+
+            } finally
+            {
+                if (inputFile != null)
+                    inputFile.close();
             }
-
-            // Hide the Loading Dialog
-            mActivity.loadingDialogHandler
-                    .sendEmptyMessage(LoadingDialogHandler.HIDE_LOADING_DIALOG);
         }
 
+    private void setVerts()
+    {
+        int num = 0;
+        try {
+            num = loadVertsFromModel("ImageTargets/banana/verts.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        mVertBuff = fillBuffer(banana_VERTS);
+        verticesNumber = num ;
     }
 
-    public void updateConfiguration()
+
+    private void setTexCoords()
     {
-        mSampleAppRenderer.onConfigurationChanged(mIsActive);
+        int num = 0;
+        try {
+            num = loadTexCoordsFromModel("ImageTargets/banana/texcoords.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        mTexCoordBuff = fillBuffer(banana_TEX_COORDS);
+
     }
 
-    // The render function called from SampleAppRendering by using RenderingPrimitives views.
-    // The state is owned by SampleAppRenderer which is controlling it's lifecycle.
-    // State should not be cached outside this method.
-    public void renderFrame(State state, float[] projectionMatrix)
+
+    private void setNorms()
     {
-        // Renders video background replacing Renderer.DrawVideoBackground()
-        mSampleAppRenderer.renderVideoBackground();
+        int num = 0;
+        try {
+            num = loadNormsFromModel("ImageTargets/banana/norms.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        mNormBuff = fillBuffer(banana_NORMS);
+    }
 
-        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
 
-        // handle face culling, we need to detect if we are using reflection
-        // to determine the direction of the culling
-        GLES20.glEnable(GLES20.GL_CULL_FACE);
-        GLES20.glCullFace(GLES20.GL_BACK);
 
-        // Did we find any trackables this frame?
-        for (int tIdx = 0; tIdx < state.getNumTrackableResults(); tIdx++) {
-            TrackableResult result = state.getTrackableResult(tIdx);
-            Trackable trackable = result.getTrackable();
-            printUserData(trackable);
-            Matrix44F modelViewMatrix_Vuforia = Tool
-                    .convertPose2GLMatrix(result.getPose());
-            float[] modelViewMatrix = modelViewMatrix_Vuforia.getData();
 
-            int textureIndex = trackable.getName().equalsIgnoreCase("stones") ? 0
-                    : 1;
-            textureIndex = trackable.getName().equalsIgnoreCase("tarmac") ? 2
-                    : textureIndex;
 
-            // deal with the modelview and projection matrices
-            float[] modelViewProjection = new float[16];
+    public int getNumObjectIndex()
+    {
+        return 0;
+    }
 
-            if (!mActivity.isExtendedTrackingActive()) {
-                /*
-                Matrix.translateM(modelViewMatrix, 0, 0.0f, 0.0f,
-                        OBJECT_SCALE_FLOAT);
-                        */
-                Matrix.translateM(modelViewMatrix, 0, 0.0f, 0.0f,
-                        OBJECT_SCALE_FLOAT);
-                Matrix.rotateM(modelViewMatrix, 0, (float) (y * 0.5), 1.0f, 0, 0);// 这是怎么了。。。。slime
-                Matrix.rotateM(modelViewMatrix, 0, (float) (x * 0.5), 0, 1.0f, 0);// 这是怎么了。。。。slime
-                Matrix.scaleM(modelViewMatrix, 0, OBJECT_SCALE_FLOAT,
-                        OBJECT_SCALE_FLOAT, OBJECT_SCALE_FLOAT);
-            } else {
-                Matrix.rotateM(modelViewMatrix, 0, 90.0f, 1.0f, 0, 0);// 这是怎么了。。。。slime
-                Matrix.scaleM(modelViewMatrix, 0, kBuildingScale,// 这又怎么了。。。。我这不还没调手势呢，你就调了干嘛呀slime
-                        kBuildingScale, kBuildingScale);
-            }
-            Matrix.multiplyMM(modelViewProjection, 0, projectionMatrix, 0, modelViewMatrix, 0);
 
-            // activate the shader program and bind the vertex/normal/tex coords
-            GLES20.glUseProgram(shaderProgramID);
+    @Override
+    public int getNumObjectVertex()
+    {
+        return verticesNumber;
+    }
 
-            if (!mActivity.isExtendedTrackingActive()) {
-                GLES20.glVertexAttribPointer(vertexHandle, 3, GLES20.GL_FLOAT,
-                        false, 0, mTeapot.getVertices());
-                GLES20.glVertexAttribPointer(textureCoordHandle, 2,
-                        GLES20.GL_FLOAT, false, 0, mTeapot.getTexCoords());
 
-                GLES20.glEnableVertexAttribArray(vertexHandle);
-                GLES20.glEnableVertexAttribArray(textureCoordHandle);
-
-                // activate texture 0, bind it, and pass to shader
-                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,
-                        mTextures.get(textureIndex).mTextureID[0]);
-                GLES20.glUniform1i(texSampler2DHandle, 0);
-
-                // pass the model view matrix to the shader
-                GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false,
-                        modelViewProjection, 0);
-
-                // finally draw the teapot
-                GLES20.glDrawElements(GLES20.GL_TRIANGLES,
-                        mTeapot.getNumObjectIndex(), GLES20.GL_UNSIGNED_SHORT,
-                        mTeapot.getIndices());
-
-                // disable the enabled arrays
-                GLES20.glDisableVertexAttribArray(vertexHandle);
-                GLES20.glDisableVertexAttribArray(textureCoordHandle);
-            } else {
-                GLES20.glDisable(GLES20.GL_CULL_FACE);
-                GLES20.glVertexAttribPointer(vertexHandle, 3, GLES20.GL_FLOAT,
-                        false, 0, mBuildingsModel.getVertices());
-                GLES20.glVertexAttribPointer(textureCoordHandle, 2,
-                        GLES20.GL_FLOAT, false, 0, mBuildingsModel.getTexCoords());
-
-                GLES20.glEnableVertexAttribArray(vertexHandle);
-                GLES20.glEnableVertexAttribArray(textureCoordHandle);
-
-                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,
-                        mTextures.get(3).mTextureID[0]);
-                GLES20.glUniformMatrix4fv(mvpMatrixHandle, 1, false,
-                        modelViewProjection, 0);
-                GLES20.glUniform1i(texSampler2DHandle, 0);
-                GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0,
-                        mBuildingsModel.getNumObjectVertex());
-
-                SampleUtils.checkGLError("Renderer DrawBuildings");
-            }
-
-            SampleUtils.checkGLError("Render Frame");
+    @Override
+    public Buffer getBuffer(BUFFER_TYPE bufferType)
+    {
+        Buffer result = null;
+        switch (bufferType)
+        {
+            case BUFFER_TYPE_VERTEX:
+                result = mVertBuff;
+                break;
+            case BUFFER_TYPE_TEXTURE_COORD:
+                result = mTexCoordBuff;
+                break;
+            case BUFFER_TYPE_NORMALS:
+                result = mNormBuff;
+                break;
+            default:
+                break;
 
         }
 
-        GLES20.glDisable(GLES20.GL_DEPTH_TEST);
-
+        return result;
     }
 
-    private void printUserData(Trackable trackable)
-    {
-        String userData = (String) trackable.getUserData();
-        Log.d(LOGTAG, "UserData:Retreived User Data	\"" + userData + "\"");
-    }
-
-
-    public void setTextures(Vector<Texture> textures)
-    {
-        mTextures = textures;
-
-    }
 
 }
